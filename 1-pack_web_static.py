28a2dff99c4922141e541deded09c65509509f89
Johnson <adebukolajohnson1@gmail.com>
2024-05-06 11:41:26 -0700

    0x03. AirBnB clone - Deploy static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive
+from the contents of the web_static folder
+"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder"""
+
+    # Create the 'versions' directory if it doesn't exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    # Generate the archive path
+    now = datetime.now()
+    archive_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+    # Compress the web_static folder into a .tgz archive
+    result = local("tar -cvzf {} web_static".format(archive_path))
+
+    # Check if the compression was successful
+    if result.failed:
+        return None
+    else:
+        return archive_path
+Symbols
+Find definitions and references for functions and other symbols in this file by clicking a symbol below or in the code.
+Filter symbols
+r
+func
+do_pack
+Footer
+
